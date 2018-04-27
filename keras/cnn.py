ab4eaff13e9c981e0bf42f4596cbebec66114d54
guillemmunozferran <guillemeetac@gmail.com>
2018-04-27 17:01:34 +0200

    Batchs and warnings

--- keras/cnn.py
+++ keras/cnn.py
@@ -1,15 +1,18 @@
 from keras.preprocessing.image import load_img, img_to_array, ImageDataGenerator
-
-img = load_img('../dataset/calle/1vuelo (1).png')
+'''
+img = load_img('../dataSet/training/street/1vuelo (72).png')
 x = img_to_array(img)
 x = x.reshape((1,) + x.shape)
 print(x.shape)
-
+'''
 #We need to split data into train, validation and test paths
-train_path
-valid_path
-test_path
+train_path = '../dataSet/training'
+valid_path = '../dataSet/validation'
+test_path = '../dataSet/testing'
 
-#ImageDataGenerator to generate a batch of images from directori
-train_batches = ImageDataGenerator().flow_from_directory(train_path, targe_size(,), classes=[],batch_size=10);
+#Image array is (144, 256, 3)
+#1034, 246 and 250 images in each directory
+train_batches = ImageDataGenerator().flow_from_directory(train_path, target_size= (144, 256), classes=['street', 'property'], batch_size=32);
+validation_batches = ImageDataGenerator().flow_from_directory(valid_path, target_size= (144,256), classes=['street', 'property'], batch_size=32);
+test_batches = ImageDataGenerator().flow_from_directory(test_path, target_size= (144,256), classes=['street', 'property'], batch_size=32);
 
