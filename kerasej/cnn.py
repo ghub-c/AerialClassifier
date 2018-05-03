8ff5c7bd732aa21b35338101dbd5b607afca3d59
guillemmunozferran <guillemeetac@gmail.com>
2018-05-04 00:24:42 +0200

    Trained model and weights

--- kerasej/cnn.py
+++ kerasej/cnn.py
@@ -1,6 +1,6 @@
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from keras.layers import Conv2D, Flatten, Dense
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
 from keras.optimizers import Adam
 
 
@@ -16,14 +16,21 @@ validation_batches = ImageDataGenerator().flow_from_directory(valid_path, target
 test_batches = ImageDataGenerator().flow_from_directory(test_path, target_size= (144,256), classes=['street', 'property'], batch_size=32);
 
 model = Sequential()
-model.add(Conv2D(32, (3, 3), strides=(3, 3), activation='relu', input_shape=(144, 256, 3)))
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(144, 256, 3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
 model.add(Dense(2, activation='softmax'))
 
 model.compile(Adam(lr=.0001), loss='categorical_crossentropy', metrics=['acc'])
 
-model.fit_generator(train_batches, steps_per_epoch=50, validation_data=validation_batches, epochs=10, verbose=1)
+model.fit_generator(train_batches, steps_per_epoch=100, validation_data=validation_batches, epochs=50, verbose=1)
 
 # Save model and weights to folder
 
-model.save_weights('./saved_models/test3.h5f')
+model.save_weights('./saved_models/final.h5f')
