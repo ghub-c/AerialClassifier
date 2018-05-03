7f0fa23496983550639e29fff6bbd1bffba79d20
guillemmunozferran <guillemeetac@gmail.com>
2018-05-03 17:17:53 +0200

    Save weights of model

--- README.md
+++ README.md
@@ -13,3 +13,6 @@ In order to run the project you need:
 - Python 2.7 or 3.5
 - Tensorflow
 ```
+
+To avoid the warning:
+pip install h5py==2.8.0rc1
--- kerasej/cnn.py
+++ kerasej/cnn.py
@@ -1,14 +1,9 @@
-from keras.preprocessing.image import load_img, img_to_array, ImageDataGenerator
+from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Conv2D, Flatten, Dense
 from keras.optimizers import Adam
 
-'''
-img = load_img('../dataSet/training/street/1vuelo (72).png')
-x = img_to_array(img)
-x = x.reshape((1,) + x.shape)
-print(x.shape)
-'''
+
 #We need to split data into train, validation and test paths
 train_path = '../dataSet/training'
 valid_path = '../dataSet/validation'
@@ -22,9 +17,13 @@ test_batches = ImageDataGenerator().flow_from_directory(test_path, target_size= 
 
 model = Sequential()
 model.add(Conv2D(32, (3, 3), strides=(3, 3), activation='relu', input_shape=(144, 256, 3)))
-model.add(Flatten()),
+model.add(Flatten())
 model.add(Dense(2, activation='softmax'))
 
-model.compile(Adam(lr=.0001), loss='mean_squared_error', metrics=['acc'])
+model.compile(Adam(lr=.0001), loss='categorical_crossentropy', metrics=['acc'])
 
 model.fit_generator(train_batches, steps_per_epoch=50, validation_data=validation_batches, epochs=10, verbose=1)
+
+# Save model and weights to folder
+
+model.save_weights('./saved_models/test3.h5f')
