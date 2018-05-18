7ded560e6ea9161fab43f1257a315a4f8af121c4
MikelG <mikel.garralaga@estudiant.upc.edu>
2018-05-18 16:24:07 +0200

    Add imread

--- imgServer/app.py
+++ imgServer/app.py
@@ -1,6 +1,9 @@
 import os
 from flask import Flask, jsonify, request
 from werkzeug.utils import secure_filename
+from scipy.misc import imread,imresize
+import numpy as np
+import cv2
 from flask_cors import CORS
 
 UPLOAD_FOLDER = './images'
@@ -14,8 +17,10 @@ def server_info():
     image = request.files['uploads[]']
     filename = secure_filename(image.filename)
     image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    im=cv2.imread('./images/'+filename)
+    print(im.shape)
     return jsonify({
-        "veredict": "veredict"
+        "veredict": "Private property"
     })
 
 if __name__ == "__main__":
