024c49164beca8bb55a50a7e8bdd44fff6dcd6f0
guillemmunozferran <guillemeetac@gmail.com>
2018-05-18 17:15:18 +0200

    Finished predictions

--- imgServer/app.py
+++ imgServer/app.py
@@ -1,10 +1,22 @@
 import os
 from flask import Flask, jsonify, request
 from werkzeug.utils import secure_filename
-from scipy.misc import imread,imresize
-import numpy as np
 import cv2
 from flask_cors import CORS
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+from keras.optimizers import Adam
+from keras.utils import plot_model
+from scipy.misc import imread, imresize
+import numpy as np
+from operator import itemgetter
+
+import sys
+sys.path.insert(0, '/Users/guillemmunozferran/Desktop/UPC/CITIES_2/AerialClassifier/keras_code')
+
+import cnn
+
 
 UPLOAD_FOLDER = './images'
 
@@ -17,8 +29,31 @@ def server_info():
     image = request.files['uploads[]']
     filename = secure_filename(image.filename)
     image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    im=cv2.imread('./images/'+filename)
-    print(im.shape)
+    img=cv2.imread('./images/'+filename)
+
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(144, 256, 3)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(2, activation='softmax'))
+
+    model.compile(Adam(lr=.0001), loss='categorical_crossentropy', metrics=['acc'])
+    model.load_weights('../keras_code/saved_models/final.h5f')
+    # model.save_weights('./saved_models/final.h5f')
+    img = np.expand_dims(img, axis=0)
+    predict = model.predict(img)
+    # predict = model.predict_generator(test_batches, steps=10, verbose=2)
+    first_class = predict[0][0]
+    second_class = predict[0][1]
+    print(first_class)
+    print(second_class)
+
     return jsonify({
         "veredict": "Private property"
     })
